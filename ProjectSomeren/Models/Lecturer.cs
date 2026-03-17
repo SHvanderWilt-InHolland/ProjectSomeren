66958baa382c946c0e52411532b0861936cb6aff
Sebastiaan van der Wilt <745982@student.inholland.nl>
2026-03-17 13:32:47 +0100

    Added age and telephone_number from the database

--- ProjectSomeren/Models/Lecturer.cs
+++ ProjectSomeren/Models/Lecturer.cs
@@ -7,5 +7,7 @@ namespace ProjectSomeren.Models
         public string LastName { get; set; }
         public string Email { get; set; }
         public string Department { get; set; }
+        public int Age { get; set; }
+        public string TelephoneNumber { get; set; }
     }
 }
